d0847ac55118a256554cf27ebd6fc89994d21cb8
Paula Mamdouh <paula.mamdouh@coformatique.com>
2019-12-31 14:35:14 +0200

    Adding basic project

---
+++ AzureTest/AzureTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AzureTest
+//
+//  Created by Dev on 31/12/2019.
+//  Copyright © 2019 Coformatique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
